f27b5b05c4c68c11e7276503bf2880f9a5830250
Liliana Aqueveque <130264861+liliaqvq@users.noreply.github.com>
2023-10-20 18:54:38 +0000

    bonito jiji

--- src/js/components/home.jsx
+++ src/js/components/home.jsx
@@ -6,14 +6,31 @@ import Board from "./Board";
 import ticTacToeTitle from "../../img/tictactoe-title.png";
 
 const Home = () => {
-	const board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
+
+	const [board, setBoard] = useState(Array(9).fill(null));
+	const [xPlaying, setXPlaying] = useState(true); //x always starts the game
+
+	const handleBoxClick = (boxIdx) => {
+		const updatedBoard = board.map((value, idx) => {
+			if (idx == boxIdx) {
+				return xPlaying === true ? "X" : "O";
+			} else {
+				return value;
+			}
+		})
+
+		setBoard(updatedBoard);
+
+		setXPlaying(!xPlaying);
+	}
+
 	return (
 		<div className="container text-center">
 			<div className="menu">
 				<img className="titleBrand" src={ticTacToeTitle} alt="Tic Tac Toe" />
 				<Announcement />
 				<StartOverButton />
-				<Board board={board} onClick={null} />
+				<Board board={board} onClick={handleBoxClick} />
 			</div>
 		</div>
 	);
